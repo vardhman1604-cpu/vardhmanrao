357e55ac4f1a4b934dc12e33e686ac319fafca6d
vardhman1604-cpu <vardhman1604@gmail.com>
2026-09-21 13:49:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Smooth scrolling and simple page animation
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function(e) {
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+            e.preventDefault();
+
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+
+    });
+
+});
